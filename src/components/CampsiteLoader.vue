785e2aaee27133f95ecd11a35e7a25dcd03fa5d4
Jess Richmond <yesjessrichmond@gmail.com>
2020-10-05 14:46:10 -0500

    (add) CampsiteLoader component

---
+++ src/components/CampsiteLoader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="CampsiteLoader">
+    <p>CAMPSITE LOADER</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CampsiteLoader"
+}
+</script>
+
+<style scoped>
+
+</style>
