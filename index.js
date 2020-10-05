e499509f69b2a3d703b002fb40025362303c4822
Christopher Booth <booth.christophermbooth@gmail.com>
2020-10-05 11:57:38 -0500

    Add basic boilerplate for Vue with proper webpack config

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const path = require('path');
+
+const app = express();
+
+app.use('/dist', express.static(path.join(__dirname, '/dist')));
 
 app.listen(8080, () => {
-  console.info('Server is listening on port 8080')
+  console.info('Server is listening on port 8080');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
