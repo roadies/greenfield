37e205865019fb58f8d74bcc76460a87dbc00816
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-10-06 10:36:40 -0500

    (add) Passport Google Authentication

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
-
 const path = require('path');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./passport-setup');
 
 const app = express();
+
+// Middleware
+
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cookieSession({
+  name: 'ontheroadagain',
+  keys: ['key1', 'key2'],
+}));
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
 
 app.use('/dist', express.static(path.join(__dirname, '/dist')));
 
@@ -12,4 +33,26 @@ app.listen(8080, () => {
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/out', (req, res) => res.send('You are logged out!'));
+app.get('/failed', (req, res) => res.send('You failed to log in!'));
+app.get('/good', (req, res) => {
+  res.send(`Welcome mr ${req.body.displayName}`);
+});
+
+app.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/good');
+  });
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/out');
 });
---
+++ passport-setup.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: 'http://localhost:8080/google/callback',
+},
+  ((accessToken, refreshToken, profile, done) => {
+    // console.log(profile);
+    done(null, profile);
+  }
+  )));
