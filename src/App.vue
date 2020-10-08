8401d43c44965877405f7fdba1fc84d22ec8e84c
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-10-08 12:14:51 -0500

    (add) navbar functionality conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
 <div id="app">
   <h1 class="app_title">On The Road Again</h1>
-  <Nav />
+  <Nav v-if="this.$route.path !== '/'" />
   <router-view></router-view>
 </div>
 </template>
 
 <script>
 import Nav from "./components/Nav.vue";
+import route from "./router";
 
 export default {
   name: "App",
