cc9734b7af5e438b0ec5aa762694774c5c6c395e
Jess Richmond <yesjessrichmond@gmail.com>
2020-10-08 01:58:17 -0500

    (add) trip router creator with input geocoding enabled

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,27 +1,86 @@
 <template>
-<gmap-map :center="{ lat: 37.0902, lng: -95.7129 }" :zoom="4" style="width: 100%; height: 500px">
-  <gmap-marker :key="i" v-for="(data, i) in childData" :position="{
-        lat: parseFloat(data.latitude),
-        lng: parseFloat(data.longitude),
-      }" :title="data.name" :clickable="true" :draggable="false" @click="
-        center = {
-          lat: parseFloat(data.latitude),
-          lng: parseFloat(data.longitude),
-        }
-      "></gmap-marker>
-</gmap-map>
+  <div>
+    <gmap-map v-show="showMap" ref="map"
+      :center="coords"
+      :zoom="5"
+      style="width: 70%; height: 500px"
+    >
+    <gmap-marker :position="this.coords"></gmap-marker>
+    <gmap-marker :position="this.destination"></gmap-marker>
+      <!-- <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position" :title="m.title" :clickable="true" :draggable="false" @click="center = m.position"></gmap-marker> -->
+  </gmap-map>
+  <button @click="getDirection">set route</button>
+  <button @click="convertToCoords">convert to coords</button>
+  {{ tripDuration }} {{ tripDistance }}
+</div>
 </template>
 
 <script>
 export default {
-  props: ["childData"],
+  // directionsService: true,
+  // directionsDisplay: true,
+  props: [
+    'origin',
+    'final',
+  ],
   data() {
     return {
-      center: {
-        lat: this.latitude,
-        lng: this.longitude,
+      showMap: true,
+      coords: {
+        lat: 29.9510555,
+        lng: -90.07148239999999,
       },
+      destination: {
+        lat: 44.3948,
+        lng: -68.0830
+      },
+      tripDuration: '',
+      tripDistance: '',
+      // markers: [{
+      //   position: {
+      //     lat: this.latitude,
+      //     lng: this.longitude,
+      //   },
+      //   title: this.title,
+      // }, ],
     };
+  },
+  methods: {
+    getDirection: function() {
+      const directionsService = new google.maps.DirectionsService;
+      const directionsDisplay = new google.maps.DirectionsRenderer;
+      directionsDisplay.setMap(this.$refs.map.$mapObject);
+      function calculateAndDisplayRoute(directionsService, directionsDisplay, start, destination) {
+        directionsService.route({
+          origin: start,
+          destination: destination,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            console.log(response.routes[0].legs[0].duration.text);
+            this.tripDuration = response.routes[0].legs[0].duration.text;
+            this.tripDistance = response.routes[0].legs[0].distance.text;
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+      calculateAndDisplayRoute(directionsService, directionsDisplay, this.coords, this.destination);
+    },
+    convertToCoords: function() {
+        console.log('PROPS', this.props);
+        const geocoder = new google.maps.Geocoder();
+        geocoder.geocode( { 'address': this.origin }, (res, status) => {
+          this.coords.lat = res[0].geometry.bounds.Ya.i;
+          this.coords.lng = res[0].geometry.bounds.Sa.i;
+        })
+        geocoder.geocode( { 'address': this.final }, (res, status) => {
+          this.destination.lat = res[0].geometry.bounds.Ya.i;
+          this.destination.lng = res[0].geometry.bounds.Sa.i;
+        })
+      }
   },
 };
 </script>
+
