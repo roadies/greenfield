680af65992e9675ee95322431a6fd86ec3ef49f9
josesrodriguez610 <josesrodriguez610@gmail.com>
2020-10-09 15:18:55 -0500

    (Add) logout functionality

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,6 +3,8 @@
   <router-link class="app_link" to="/newtrip">Create a Trip</router-link>
   <router-link class="app_link" to="/profile">My Trips</router-link>
   <router-link class="app_link" to="/journal">Journal</router-link>
+
+  <a class="home_sign_in" href="/logout">logout</a>
 </nav>
 </template>
 
