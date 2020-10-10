774f5020e3da95bbfd7181016e67cec9dcd6abbf
Jess Richmond <yesjessrichmond@gmail.com>
2020-10-10 17:25:27 -0500

    (add) connect Trip component in Profile view to SavedTrip view

--- src/components/Trip.vue
+++ src/components/Trip.vue
@@ -3,13 +3,16 @@
   <div class="trip_container" v-for="(trip, i) in trips.slice().reverse()" :key="i">
     <h3>{{ trip.name }}</h3>
     <h4>{{ trip.location_start }} - {{ trip.location_end }}</h4>
+    <h5>{{trip.start_date}}</h5>
     <JournalForm :trip="trip"/>
+    <router-link class="app_link" :to="{ path: '/trip', query: { id: trip.id }}"><button>view trip</button></router-link>
   </div>
 </div>
 </template>
 
 <script>
 import JournalForm from './JournalForm.vue';
+// import ViewTrip from ''
 export default {
   name: "Trip",
   props: ["trips"],
