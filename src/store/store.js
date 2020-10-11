1ba754de7bfdce06ab0efcfbf7782a1f4f9a402c
christophermbooth <booth.christopherm@gmail.com>
2020-10-11 10:02:42 -0500

    (add) New state values

--- src/store/store.js
+++ src/store/store.js
@@ -9,6 +9,8 @@ const store = new Vuex.Store({
     isUserLoggedIn: false,
     userName: '',
     userId: '',
+    newestFileUpload: '',
+    tripCalculated: false,
     imageLink: '',
   },
   getters: {
